6c929331d508c48f3c12a7b94369eef9b428315d
J.Cole <ccjohn015@gmail.com>
2021-12-09 10:35:19 -0600

    super framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 var password=document.getElementById("password");
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 12;
-var password = "";
 
 for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random() * chars.length);
@@ -16,9 +13,11 @@ function genPassword() {
     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var passwordLength = 12;
     var password = "";
-for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-document.getElementById("password").value = password;
+
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+    }
+    
+    document.getElementById("password").value = password;
 }
